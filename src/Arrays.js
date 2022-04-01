27de106aca74ad8e7e4364ab41c81009b6300bfc
huscker <egostalker03@mail.ru>
2022-04-01 20:00:54 +0300

    completed task

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,41 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+	let numbers = data.split(',')
+	numbers.map(el=>{
+		return parseInt(el);
+	});
+	numbers.sort((a,b)=>{
+		return a-b;
+	});
+	return numbers.join(',');
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+	return data.filter(el=>{
+		return el <= 100;
+	});
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+	let array3 = [];
+	for (let i = 0;i<10;i++){
+		if(i % 2 == 0){
+			array3.push(array1[i/2]);
+		}else{
+			array3.push(array2[Math.floor(i/2)]);
+		}
+	}
+	return array3;
+}
